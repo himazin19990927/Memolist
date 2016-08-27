d019f11f63f0270f813dfaac7224cb4bebdfc930
原田大樹 <himazin0927@gmail.com>
2016-08-27 20:00:38 +0900

    Initial Commit

---
+++ Memolist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Memolist
+//
+//  Created by 原田大樹 on 2016/08/27.
+//  Copyright © 2016年 原田大樹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
